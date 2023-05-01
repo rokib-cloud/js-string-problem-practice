b0f10a49c3541ac0e5eff9760f00c8915b9f62e3
ROKIB HOSSAIN <rokibh393@gmail.com>
2023-05-01 19:41:53 +0600

    case done

---
+++ case.js
@@ -0,0 +1,8 @@
+const userName = 'blackPink';
+const userInput = 'blackPink';
+if (userName.toLocaleLowerCase === userInput.toLocaleLowerCase) {
+    console.log('valid user')
+}
+else {
+    console.log('invalid user')
+}
