08a59cb076f467ef61a3b347b3ea1f3928c439c7
ROKIB HOSSAIN <rokibh393@gmail.com>
2023-05-02 09:17:34 +0600

    temp.js

---
+++ temp.js
@@ -0,0 +1,15 @@
+let first = 5;
+let second = 7;
+// console.log(first, second);
+
+
+// approach :1
+// const temp = first
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// approach :2
+[first, second] = [second, first];
+console.log(first, second);
+
