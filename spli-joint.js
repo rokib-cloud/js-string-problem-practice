4808a9b223e095638d30b03bb79d6d8f0da7857f
ROKIB HOSSAIN <rokibh393@gmail.com>
2023-05-02 08:52:03 +0600

    spli-joint.js

---
+++ spli-joint.js
@@ -0,0 +1,12 @@
+const lyrics = 'tmi kala pakhi ami jani ki. bosonto kale tomay bolte pari ni';
+
+const parts = lyrics.split(' ');
+const sentences = lyrics.split('.');
+const char = lyrics.split('');
+// console.log(parts)
+// console.log(sentences);
+// console.log(char);
+
+const partial = lyrics.slice(5, 8);
+const partial2 = lyrics.substring(5, 8);
+console.log(partial2)
