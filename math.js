5bb50244b417873e4f72e9ad0b26ca18ff374396
ROKIB HOSSAIN <rokibh393@gmail.com>
2023-05-02 09:08:20 +0600

    math.js

---
+++ math.js
@@ -0,0 +1,33 @@
+const result = Math.pow(2, 2);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+if (gap < 5) {
+    console.log('you guys can be friends');
+}
+else {
+    console.log('you guys stay apart');
+}
+
+
+
+// ----------------------
+//
+//------------------------
+const num = 2.4598;
+const fullNum = Math.round(num);
+// console.log(fullNum);
+const result2 = Math.ceil(2.00001);
+const result3 = Math.floor(2.00001);
+// console.log(result3)
+
+// console.log(Math.random());
+
+for (let i = 0; i < 20; i++) {
+    const random = Math.round(Math.random() * 6);
+    console.log(random);
+}
+
